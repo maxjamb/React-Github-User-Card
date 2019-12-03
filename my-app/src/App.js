badafcdd6ebc40a77632ac2f2642e87f08e2dc7e
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-12-03 15:13:28 +0000

    CRA and axios github user data

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/maxjamb/followers")
+      .then(res => {
+        console.log(res);
+        setUsers(res.data);
+      })
+      .catch(error => {
+        console.log("data not returned", error);
+      });
+  }, []);
+  return (
+    <div className="App">
+      {users
+        ? users.map(user => {
+            return <h1>{user.login}</h1>;
+          })
+        : []}
+    </div>
+  );
+}
+
+export default App;
